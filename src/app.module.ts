943269346c7a2d6da4bc199633f818968800f323
Francis Lagares <francis.lagares@gmail.com>
2022-05-24 18:24:40 +0200

    feat: global config and prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
 })
 export class AppModule {}
